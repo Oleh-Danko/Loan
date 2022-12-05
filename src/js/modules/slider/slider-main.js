e041237623f9bf0093f18077cc02ace6402d851d
Oleh <danko.oleh@gmail.com>
2022-12-05 18:51:53 +0200

    sliders finish

--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -1,9 +1,8 @@
-export default class Slider {
-    constructor(page, btns){
-        this.page = document.querySelector(page);
-        this.slides = this.page.children;
-        this.btns = document.querySelectorAll(btns);
-        this.slideIndex = 1;
+import Slider from "./slider";
+
+export default class MainSlider extends Slider {
+    constructor(btns) {
+        super(btns);
     }
 
     showSlides(num) {
@@ -17,9 +16,8 @@ export default class Slider {
 
         try {
             this.hanson.style.opacity = '0';
-            this.hanson.classList.add('animated');
-
             if (num === 3) {
+                this.hanson.classList.add('animated');
                 setTimeout(() => {
                     this.hanson.style.opacity = '1';
                     this.hanson.classList.add('slideInUp');
@@ -30,6 +28,7 @@ export default class Slider {
         } catch(e) {}
 
         this.slides.forEach(slide => {
+            // slide.classList.add('animated', 'slideInDown');
             slide.style.display = 'none';
         });
 
